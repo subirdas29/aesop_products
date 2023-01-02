023b271d6885fa5668ab159afe3ce066601c0942
subirdas29 <dasjoy104@gmail.com>
2023-01-02 20:13:53 +0600

    privacypolicy added

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrivacyPolicy from '../PrivacyPolicy/PrivacyPolicy';
+
+const Home = () => {
+    return (
+        <div>
+            <PrivacyPolicy></PrivacyPolicy>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Home/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className='bg-[#A6672B] py-2'>
+            <p className='text-center text-[#FFFEF2] text-base'>Our privacy policy has been updated effective 31 August. 
+            <a href='#' className='hover:underline'> Read privacy policy</a></p>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
